2e9d1508ce551190434a2484f3b2ed5346a105ce
Pedro Fernando <pedropalmieri_13@hotmail.com>
2016-04-05 21:24:27 -0300

    Melhorias no formulário
    Adicionado mais efeitos nos campos com erro
    Adicionado Estado Civil

--- modulos/formulario/formulario.controller.js
+++ modulos/formulario/formulario.controller.js
@@ -3,4 +3,28 @@ angular
   .controller('FormularioController', FormularioController);
 
 function FormularioController($scope) {
+  var vm = this;
+
+  vm.onChangeSexo = function() {
+    if (vm.Sexo == 'F') {
+      vm.enumEstadoCivil = {
+        Solteiro: "Solteira",
+        Casado: "Casada",
+        Viuvo: "Viúva",
+        Separado: "Separada",
+        Divorciado: "Divorciada"
+      };
+    } else {
+      vm.enumEstadoCivil = {
+        Solteiro: "Solteiro",
+        Casado: "Casado",
+        Viuvo: "Viúvo",
+        Separado: "Separado",
+        Divorciado: "Divorciado"
+      };
+    }
+  };
+
+  vm.onChangeSexo();
+
 };
