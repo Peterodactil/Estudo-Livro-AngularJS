8b92060afb2520849d4c601f3318b07adf85a514
Pedro Fernando <pedropalmieri_13@hotmail.com>
2016-06-02 21:51:09 -0300

    Ajustada as rotas das paginas

--- js/app.routes.js
+++ js/app.routes.js
@@ -1,25 +1,27 @@
 angular
     .module('app')
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {
+        var site_prefix = '/Estudo-Livro-AngularJS';
+
+        $routeProvider.when(site_prefix + '/', {
             template: '<h3>Página Inicial</h3>'
         })
-        .when('/basico', {
+        .when(site_prefix + '/basico', {
             templateUrl: '/modulos/basico/basico.html',
             controller: 'BasicoController',
             controllerAs: 'basicoCtrl'
         })
-        .when('/diretiva', {
+        .when(site_prefix + '/diretiva', {
             templateUrl: '/modulos/diretiva/diretiva.html',
             controller: 'DiretivaController',
             controllerAs: 'diretivaCtrl'
         })
-        .when('/formulario', {
+        .when(site_prefix + '/formulario', {
             templateUrl: '/modulos/formulario/formulario.html',
             controller: 'FormularioController',
             controllerAs: 'formCtrl'
         })
-        .when('/filtro', {
+        .when(site_prefix + '/filtro', {
             templateUrl: '/modulos/filtro/filtro.html',
             controller: 'FiltroController',
             controllerAs: 'filtroCtrl'
