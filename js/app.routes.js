5ab4a55b13a2ddd7d090c61dca367773fc5f5dc0
Pedro Fernando <pedropalmieri_13@hotmail.com>
2016-06-02 21:53:33 -0300

    Correção rota das paginas

--- js/app.routes.js
+++ js/app.routes.js
@@ -7,22 +7,22 @@ angular
             template: '<h3>Página Inicial</h3>'
         })
         .when(site_prefix + '/basico', {
-            templateUrl: '/modulos/basico/basico.html',
+            templateUrl: 'modulos/basico/basico.html',
             controller: 'BasicoController',
             controllerAs: 'basicoCtrl'
         })
         .when(site_prefix + '/diretiva', {
-            templateUrl: '/modulos/diretiva/diretiva.html',
+            templateUrl: 'modulos/diretiva/diretiva.html',
             controller: 'DiretivaController',
             controllerAs: 'diretivaCtrl'
         })
         .when(site_prefix + '/formulario', {
-            templateUrl: '/modulos/formulario/formulario.html',
+            templateUrl: 'modulos/formulario/formulario.html',
             controller: 'FormularioController',
             controllerAs: 'formCtrl'
         })
         .when(site_prefix + '/filtro', {
-            templateUrl: '/modulos/filtro/filtro.html',
+            templateUrl: 'modulos/filtro/filtro.html',
             controller: 'FiltroController',
             controllerAs: 'filtroCtrl'
         })
