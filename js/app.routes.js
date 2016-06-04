956367043a95f2d95ee599dfdeee3613b9a1b442
Pedro Fernando <pedropalmieri_13@hotmail.com>
2016-06-04 13:00:15 -0300

    Ajuste nas rotas

--- js/app.routes.js
+++ js/app.routes.js
@@ -1,28 +1,28 @@
 angular
     .module('app')
     .config(function($routeProvider) {
-        var site_prefix = this.document.location.pathname.replace('/', '');
+        var site_prefix = this.document.location.pathname;
 
         $routeProvider.when('/', {
             template: '<h3>Página Inicial</h3>'
         })
         .when(site_prefix + '/basico', {
-            templateUrl:  site_prefix + '/modulos/basico/basico.html',
+            templateUrl:  site_prefix + 'modulos/basico/basico.html',
             controller: 'BasicoController',
             controllerAs: 'basicoCtrl'
         })
         .when('/diretiva', {
-            templateUrl:  site_prefix + '/modulos/diretiva/diretiva.html',
+            templateUrl:  site_prefix + 'modulos/diretiva/diretiva.html',
             controller: 'DiretivaController',
             controllerAs: 'diretivaCtrl'
         })
         .when('/formulario', {
-            templateUrl:  site_prefix + '/modulos/formulario/formulario.html',
+            templateUrl:  site_prefix + 'modulos/formulario/formulario.html',
             controller: 'FormularioController',
             controllerAs: 'formCtrl'
         })
         .when('/filtro', {
-            templateUrl: site_prefix + '/modulos/filtro/filtro.html',
+            templateUrl: site_prefix + 'modulos/filtro/filtro.html',
             controller: 'FiltroController',
             controllerAs: 'filtroCtrl'
         })
